8a79b2ee60e5fe84ca68d5248151a19b8ee70ac5
Jorge Valenzuela <valenzuelajorge@gmail.com>
2026-04-16 14:56:16 -0500

    fix: main.jsx render StudentApp as entry point for role-based routing

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App'
+import StudentApp from './views/StudentApp'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <StudentApp />
   </StrictMode>,
 )
